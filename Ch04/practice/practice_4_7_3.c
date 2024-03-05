23f320828a134f1540fb7126d8b41b170e8d1cef
itzixiao <itzixiao@foxmail.com>
2024-03-05 17:13:12 +0800

    fix(): adjust directory

--- Ch04/practice/practice_4_7_3.c
+++ Ch04/practice/practice_4_7_3.c
@@ -2,11 +2,11 @@
 // Created by Lenovo on 2024/3/4.
 //
 #include <stdio.h>
-#include <string.h.>
+#include <string.h>
 
 #define Q "His Hamlet was funny without being vulgar."
 
 int main(void) {
-    printf("%s\nhas %d characters.", Q, strlen(Q));
-    printf("\"%s\"\nhas %d characters.", Q, strlen(Q));
+    printf("%s\nhas %d characters.\n", Q, strlen(Q));
+    printf("\"%s\"\nhas %d characters.\n", Q, strlen(Q));
 }
