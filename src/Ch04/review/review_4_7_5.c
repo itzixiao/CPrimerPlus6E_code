8c24a3ff41f50732387bdd7225284e6c3626ee66
itzixiao <itzixiao@foxmail.com>
2024-03-06 10:59:09 +0800

    fix(): adjust directory

---
+++ src/Ch04/review/review_4_7_5.c
@@ -0,0 +1,17 @@
+//
+// Created by Lenovo on 2024/3/4.
+//
+#include <stdio.h>
+
+#define BOOK "War and peace"
+
+int main(void ) {
+
+    float cost = 12.99;
+    float percent = 80.0;
+    printf("This copy of \"%s\" sells for $%.2f.\n", BOOK, cost);
+
+    printf("That is %.0f%% of list.\n", percent);
+
+    return 0;
+}
